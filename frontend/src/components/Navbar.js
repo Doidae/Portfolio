682f9c5b64ac7579898a1059d292f86a8c12d2f5
Doidae <andyarredondo1224@gmail.com>
2024-07-29 17:42:22 -0400

    Added css styling and icon

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './style/Navbar.css'
 import { NavLink } from 'react-router-dom'
+import Icon from './images/portfolio-icon.gif'
 
 const Navbar = () =>{
     return(
-        <nav>
+        <nav className='navbar'>
             <div>
+                <img src={Icon} alt='Portfolio Icon' className='navbar-logo' exact to='/'/>
+            </div>
+            <div className='navbar-right'>
                 <NavLink exact to='/' activateClassName='active' className='navbar-link'>Home</NavLink>
                 <NavLink exact to='/about' activateClassName='about' className='navbar-link'>About</NavLink>
                 <NavLink exact to='/projects' activateClassName='projects' className='navbar-link'>Projects</NavLink>
